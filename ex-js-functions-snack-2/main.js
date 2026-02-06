054a7aeb9bed46b9f6310f35d45a100a8ccec9ab
AndreaMeroi <andreameroidublin@gmail.com>
2026-02-06 18:02:49 +0100

    add adavanced function

---
+++ ex-js-functions-snack-2/main.js
@@ -0,0 +1,13 @@
+// Crea una funzione dichiarativa chiamata somma che accetta due numeri e restituisce la loro somma.
+function somma(numero_1, numero_2) {
+    return numero_1 + numero_2
+}
+
+// Poi, definisci la stessa funzione somma ma come funzione anonima assegnata a una variabile
+const sum = somma(3, 8)
+console.log(sum);
+
+
+
+//Quindi, riscrivi la funzione somma con la sintassi delle arrow functions.
+() => numero_1 + numero_2 
