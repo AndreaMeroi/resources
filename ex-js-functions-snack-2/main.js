fd61627a0ff4d927bb6095178e115d132539d1a5
AndreaMeroi <andreameroidublin@gmail.com>
2026-02-10 09:44:02 +0100

    add ex functions

--- ex-js-functions-snack-2/main.js
+++ ex-js-functions-snack-2/main.js
@@ -2,12 +2,16 @@
 function somma(numero_1, numero_2) {
     return numero_1 + numero_2
 }
+console.log(somma(3, 8));
+
 
 // Poi, definisci la stessa funzione somma ma come funzione anonima assegnata a una variabile
-const sum = somma(3, 8)
-console.log(sum);
-
-
+const sum = function () {
+    return 1 + 5
+}
+console.log(sum());
 
 //Quindi, riscrivi la funzione somma con la sintassi delle arrow functions.
-() => numero_1 + numero_2 
+
+const summ = () => 1 + 8
+console.log(summ());
